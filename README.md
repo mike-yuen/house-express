ee4780f1ec77c24d34a244c96026e6c8cf42fa7b
Minh Nguyen <minh.nguyen@hdwebsoft.co>
2020-11-16 16:55:48 +0700

    NC-01 initialize code

---
+++ README.md
@@ -0,0 +1,25 @@
+# Crypto Node.js project 🛡️
+
+## Development
+
+We use `node` version `14.9.0`
+
+```
+nvm install 14.9.0
+```
+
+```
+nvm use 14.9.0
+```
+
+The first time, you will need to run
+
+```
+npm install
+```
+
+Then just start the server with
+
+```
+npm run start
+```
---
+++ src/api/index.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import auth from './routes/auth';
+import user from './routes/user';
+import agendash from './routes/agendash';
+
+// guaranteed to get dependencies
+export default () => {
+	const app = Router();
+	auth(app);
+	user(app);
+	agendash(app);
+
+	return app
+}
