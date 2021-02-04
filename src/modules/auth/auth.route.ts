39c785ccb0a0ab23bb318a02417d042180adbdca
Minh Nguyen <minh.nguyen@hdwebsoft.co>
2021-02-04 17:10:37 +0700

    NC-02 update auth flow

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,63 +1,37 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Router } from 'express';
 import { Container } from 'typedi';
-import AuthService from './auth.service';
-import { IUserInputDTO } from '../users/user.interface';
 import middlewares from './middlewares';
-import { Logger } from 'winston';
 import * as userSchema from './auth.validator';
+import AuthController from './auth.controller';
 
 const route = Router();
 
 export default (app: Router) => {
   app.use('/auth', route);
-
+  const authController: AuthController = Container.get(AuthController);
   // @POST: /signup
-  route.post('/signup', userSchema.SignUpSchema, async (req: Request, res: Response, next: NextFunction) => {
-    const logger: Logger = Container.get('logger');
-    logger.debug('Calling Sign-Up endpoint with body: %o', req.body);
-    try {
-      const authServiceInstance = Container.get(AuthService);
-      const { user, token } = await authServiceInstance.SignUp(req.body as IUserInputDTO);
-      return res.status(201).json({ user, token });
-    } catch (e) {
-      logger.error('🔥 error: %o', e);
-      return next(e);
-    }
-  });
+  route.post('/signup', userSchema.SignUpSchema, authController.SignUp);
 
-  // @POST: /signin
-  route.post('/signin', userSchema.SignInSchema, async (req: Request, res: Response, next: NextFunction) => {
-    const logger: Logger = Container.get('logger');
-    logger.debug('Calling Sign-In endpoint with body: %o', req.body);
-    try {
-      const { email, password } = req.body;
-      const authServiceInstance = Container.get(AuthService);
-      const { user, token } = await authServiceInstance.SignIn(email, password);
-      return res.json({ user, token }).status(200);
-    } catch (e) {
-      logger.error('🔥 error: %o', e);
-      return next(e);
-    }
-  });
+  // // @POST: /signin
+  route.post('/signin', userSchema.SignInSchema, authController.SignIn);
 
-  /**
-   * @TODO Let's leave this as a place holder for now
-   * The reason for a logout route could be deleting a 'push notification token'
-   * so the device stops receiving push notifications after logout.
-   *
-   * Another use case for advance/enterprise apps, you can store a record of the jwt token
-   * emitted for the session and add it to a black list.
-   * It's really annoying to develop that but if you had to, please use Redis as your data store
-   */
-  route.post('/logout', middlewares.isAuth, (req: Request, res: Response, next: NextFunction) => {
-    const logger: Logger = Container.get('logger');
-    logger.debug('Calling Sign-Out endpoint with body: %o', req.body);
-    try {
-      //@TODO AuthService.Logout(req.user) do some clever stuff
-      return res.status(200).end();
-    } catch (e) {
-      logger.error('🔥 error %o', e);
-      return next(e);
-    }
-  });
+  // /**
+  //  * @TODO Let's leave this as a place holder for now
+  //  * The reason for a logout route could be deleting a 'push notification token'
+  //  * so the device stops receiving push notifications after logout.
+  //  *
+  //  * Another use case for advance/enterprise apps, you can store a record of the jwt token
+  //  * emitted for the session and add it to a black list.
+  //  * It's really annoying to develop that but if you had to, please use Redis as your data store
+  //  */
+  // route.post('/logout', middlewares.isAuth, (req: Request, res: Response, next: NextFunction) => {
+  //   logger.debug('Calling Sign-Out endpoint with body: %o', req.body);
+  //   try {
+  //     //@TODO AuthService.Logout(req.user) do some clever stuff
+  //     return res.status(200).end();
+  //   } catch (e) {
+  //     logger.error('🔥 error %o', e);
+  //     return next(e);
+  //   }
+  // });
 };
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,40 +1,38 @@
 import { Service, Inject } from 'typedi';
 import jwt from 'jsonwebtoken';
 // import MailerService from './mailer';
-import config from '../../config';
-import argon2 from 'argon2';
-import { IUserOutputDTO, IUserInputDTO } from '../users/user.interface';
-import { EventDispatcher, EventDispatcherInterface } from '../../decorators/eventDispatcher';
-import events from '../../subscribers/events';
+import config from '@/config';
+import { IUserOutputDTO, IUserInputDTO } from '@/modules/users/user.interface';
+import { EventDispatcher, EventDispatcherInterface } from '@/decorators/eventDispatcher';
+import events from '@/subscribers/events';
 import UserRepository from '@/modules/users/user.repository';
+import { Logger } from 'winston';
 
 @Service()
 export default class AuthService {
   constructor(
     // private mailer: MailerService,
-    private userRepository: UserRepository,
-    @Inject('logger') private logger,
+    private readonly userRepository: UserRepository,
+    @Inject('logger') private logger: Logger,
     @EventDispatcher() private eventDispatcher: EventDispatcherInterface,
   ) {}
 
-  public async SignUp(userInputDTO: IUserInputDTO): Promise<{ user: IUserOutputDTO; token: string }> {
+  public SignUp = async (userInputDTO: IUserInputDTO): Promise<{ user: IUserOutputDTO; token: string }> => {
     try {
       const user = await this.userRepository.SignUp(userInputDTO);
-
       const token = this.generateToken(user);
       // this.logger.silly('Sending welcome email');
       // await this.mailer.SendWelcomeEmail(userRecord);
-
       this.eventDispatcher.dispatch(events.user.signUp, { user });
 
       return { user, token };
     } catch (e) {
-      this.logger.error(e);
+      this.logger.error('🔥 error %o', e);
       throw e;
     }
-  }
+  };
 
-  public async SignIn(email: string, password: string): Promise<{ user: IUserOutputDTO; token: string }> {
+  public SignIn = async (email: string, password: string): Promise<{ user: IUserOutputDTO; token: string }> => {
     try {
       const user = await this.userRepository.SignIn(email, password);
       const token = this.generateToken(user);
@@ -43,9 +41,9 @@ export default class AuthService {
       this.logger.error(e);
       throw e;
     }
-  }
+  };
 
-  private generateToken(user) {
+  private generateToken = user => {
     const today = new Date();
     const exp = new Date(today);
     exp.setDate(today.getDate() + 60);
@@ -59,5 +57,5 @@ export default class AuthService {
       },
       config.jwtSecret,
     );
-  }
+  };
 }
